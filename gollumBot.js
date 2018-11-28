089e3e3d6e763f285f19e7c090e541c9640fed4f
molecs <45439213+molecs@users.noreply.github.com>
2018-11-28 23:52:37 +0200

    Create gollumBot.js

---
+++ gollumBot.js
@@ -0,0 +1,28 @@
+const discord = require ('discord.js')
+
+var client = new discord.Client();
+
+const token = "NTE3NDIzMzE1MTcwNzU0NTgx.DuCG3Q.n4xG7BzB-3UNd3Qa6NcrfFFmryI";
+
+client.on ("ready", () => {
+    console.log ("ready!");
+
+    client.user.setGame ("Luikumassa nyöriin!");
+});
+
+const prefix = "klonkku";
+client.on ("message", (message) => {
+
+    if (message.author.bot) return;
+
+    if (message.content.startsWith (prefix + " kys")) {
+        message.reply ('kys');
+    }
+
+    if (message.content.startsWith ("klonkku")) {
+        message.channel.send  ({files: ["./kuvat/klonkkutenkasja.png"]})
+    }
+
+});
+
+client.login (process.env.BOT_TOKEN);
